6e536e88cc54c1c1302c658a33696a353f315b5e
D <106179965+DhirajMore1@users.noreply.github.com>
2023-11-09 15:01:15 +0530

    Update streamlit_app.py
    added 3 lines of menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner');
+streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+st.text("Grilled salmon with quinoa and steamed vegetables.")
+st.text("Mango avocado salad with grilled chicken and honey mustard dressing.")
+st.text("Chocolate chip pancakes topped with bananas and whipped cream.")
