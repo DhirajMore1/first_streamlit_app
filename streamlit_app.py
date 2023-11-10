e20b34920a9ae708d6c3ca7dcd0a499bc6895c83
D <106179965+DhirajMore1@users.noreply.github.com>
2023-11-10 15:03:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,4 @@ streamlit.text("Grilled salmon with quinoa and steamed vegetables.")
 streamlit.text("Mango avocado salad with grilled chicken and honey mustard dressing.")
 streamlit.text("Chocolate chip pancakes topped with bananas and whipped cream.")
 
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
