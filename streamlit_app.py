865ff2215aaac77a35c782bd2e774357f6e6f012
D <106179965+DhirajMore1@users.noreply.github.com>
2023-11-10 14:58:48 +0530

    Update streamlit_app.py
    Created another header for a special menu item

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,6 @@ streamlit.header('Breakfast Menu')
 streamlit.text("Grilled salmon with quinoa and steamed vegetables.")
 streamlit.text("Mango avocado salad with grilled chicken and honey mustard dressing.")
 streamlit.text("Chocolate chip pancakes topped with bananas and whipped cream.")
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
